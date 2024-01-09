ea591f3e0dc6ca98dbbcf210890d4d2cc49bafc4
MaggyQuijano <magali.quijano.pliego@gmail.com>
2024-01-09 17:11:00 -0600

    se agrega el primer ejercicio

---
+++ ejercicios3.js
@@ -0,0 +1,14 @@
+// Ejercicios para esta sesion
+
+/** Ejercicio 1
+ * 
+ * Elabora un programa que determine si una persona tiene edad
+ * suficiente para votar
+ * 
+ * Debe recibir como parametro la edad
+ * 
+ * Consideraciones: utilizar la estructura if-else
+ */
+
+
+function edad();
