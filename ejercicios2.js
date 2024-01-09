980638fcb5016f38a04d4b9bacdfa2f691871773
MaggyQuijano <magali.quijano.pliego@gmail.com>
2024-01-09 15:05:30 -0600

    seterminann funciones

--- ejercicios2.js
+++ ejercicios2.js
@@ -8,6 +8,8 @@ function perimetroDeCirculo( radio){
     return `El perimetro del circulo es : ${2 *pi* radio}`;
 }
 
+// const perimetroDeCirculo = (radio) => ()
+
 // * calcular el area de un rectangulo
 
 function areaDeRectangulo (b,a){
@@ -20,3 +22,20 @@ function cuadradoDeUnNumero (a){
 return `El cuadrado de equis número es : ${a*a}`;}
 
 //calcular la conversion de grados celsius a farenheit
+function conversionCelsiusAFahrenheit (grados){
+    let farenheit = (grados*9/5 + 32)
+
+    return `La conversion de grados Celsius a Farenheit son : ${farenheit}`
+}
+
+const conversionCelsiusAFahrenheit = grados => (grados*9/5+32)
+
+console.log("La conversion de grados celsius a Farenheit son: "+ grados); 
+
+//* calcular el valor del voltaje dadas la resistencia y la corriente 
+
+function valorVoltaje(I,R){
+    let voltaje = I*R;
+    return ` El voltaje es : ${ voltaje }`;
+}
+
