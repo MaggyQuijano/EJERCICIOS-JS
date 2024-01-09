bbc620d7940f7c42f9102cae2c1f34082cfb9a1b
MaggyQuijano <152654713+MaggyQuijano@users.noreply.github.com>
2024-01-08 18:25:49 -0600

    Add files via upload

---
+++ variables.js
@@ -0,0 +1,264 @@
+/** Ejercicio para esta sesion
+ * 
+ * Declarar 10 variables de cada tipo de dato sin tomar en cuenta objetos, arrays y null
+ * Pueden variar entre las palabras reservadas var, let y const
+ *      -number
+ *      -string
+ *      -boolean
+ * 
+ * Declarar variables numericas y realizar las operaciones aritmeticas basicas
+ *      -suma
+ *      -resta
+ *      -multiplicacion
+ *      -division
+ * 
+ * Declarar variables que concatenen cadenas de texto
+ *      -5 mensajes diferentes en pantalla, sin limite de palabras.
+ * 
+ * Declarar 5 variables y convertirlas a otro tipo de dato
+ * 
+ *  CONSIDERACIONES: poner en practica las mejores practicas para declaracion de variables.
+ * Imprimir los resultados en pantalla con un console.log()
+ */
+
+
+var edadPersona1 = 22 ;
+let edadPersona2 = 15 ;
+let edadPersona3 = 44;
+var edadPersona4 = 11;
+var edadPersona5 = 26;
+let edadPersona6 =32;
+let  edadPersona7 =29;
+var edadPersona8 = 42;
+let edadPersona9 =58;
+let edadPersona10 = 62;
+
+
+
+let libroFavorito = "Donde habitan los angeles";
+let deporteFav = "Soccer";
+var nombreMascota =" Canica"
+let paisNacimiento = "Mexico"
+var comidaFav =" Enfrijoladas"
+let nacionalidad = "Mexicana"
+var sexo ="Femenino"
+let colorFav = "Rosa"
+var  idioma = "Español"
+let correo = "magali.quijano.pliego@gmail.com"
+
+let tengo20Años = false;
+var soyHombre =false;
+let estudio = true;
+var soyMujer = true;
+let habloIngles = false;
+var soyProgramadora = true;
+let tengoHermanos = true;
+let soyMayorDeEdad = true;
+let soyVegetariana = false;
+let soyUnGato = false;
+
+
+
+
+
+
+
+// *Declarar variables numericas y realizar las operaciones aritmeticas basicas
+//  *      -suma
+//  *      -resta
+//  *      -multiplicacion
+//  *      -division
+//  * 
+let num1 = 20;
+var num2 =25;
+let suma = num1+num2; 
+
+console.log(suma);
+
+let num3 =500;
+var num4 =324;
+let resta = num3-num4;
+
+console.log(resta)
+
+let num5 = 958;
+var num6 = 2458;
+let multiplicacion = num5*num6;
+
+console.log(multiplicacion);
+
+let num7 =9523;
+var num8 = 652;
+let division = num7/num8;
+
+console.log(division);
+
+// * Declarar variables que concatenen cadenas de texto
+// *      -5 mensajes diferentes en pantalla, sin limite de palabras.
+
+let nombre = "Magali";
+let apellidoP = "Quijano";
+var apellidoM = "Pliego";
+var profesion = "Desarrolladora";
+let direccion = "Nicolas Romero";
+
+console.log("Nombre: " + nombre)
+
+console.log("Apellido Paterno: "+ apellidoP)
+
+console.log("Apellido Materno: " + apellidoM)
+
+console.log( "Profesion: " + profesion)
+
+console.log( "Dirección: " + direccion)
+
+// * Declarar 5 variables y convertirlas a otro tipo de dato
+
+let pan = 10;
+let leche = 7;
+let embutido = "Jamon";
+let pagoEfectivo = true ;
+let huevos = 5;
+
+let panString = String(pan);
+let lecheString = String(leche);
+let embutidoNumber = Number(embutido);
+let pagoEfectivoNumber = Number(pagoEfectivo);
+let huevosBoolean = Boolean(huevos);
+
+
+console.log(panString,lecheString,embutidoNumber, pagoEfectivoNumber,huevosBoolean);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// var amIAlive = true;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// comentario de una sola línea
+
+/** esto es
+ * un comentario
+ * de  varias
+ * lineas en JS
+ */
+
+// VARIABLE NUMERICA
+
+// var edadHector = 25;
+// edadHector = 30; 
+
+
+// //console.log("La edad de Hector es: " + edadHector);
+
+
+// let denisseYearOfBirth = 1995; 
+// //console.log("Denisse was born in: " + denisseYearOfBirth);
+
+// let suma = edadHector + denisseYearOfBirth;
+// //console.log("la suma de la edad de hector y el año de nacimiento de Denisse es : " + suma)
+
+// // VARIABLES DE TIPO STRING comidas dobles o simples 
+
+// let postreFavorito = "Helado";
+
+// var movieFav = " Star Wars";
+
+// //VARIABLE DE TIPO BOOLEANO  TRUE AND FALSE
+
+// let miNombreEsDenisse = false;
+
+// var amIAlive = true;
+
+// // VARIABLE DE TIPO NULO (NULL)
+
+// let datoVacio = null;
+// let datoVacio1 = "";
+
+// // VARIABLES DE TIPO ARRAY  
+
+// let coleccionDeCoches = ["Porsche", "Mercedes-Benz", "Ferrari", "Nissan"];
+
+// let frutas = ["Manzana", "Plátano", "Mango"];
+
+// let chuches = ["chips moradas", "takis verdes", "huevito kinder", "churritos con salsa"];
+
+// let nuevoArreglo = [ ]
+
+
+// // Variables de tipo objeto 
+
+// // let libro = { 
+// //     nombre: "El mundo de Sofia" ,
+// //     editorial: "ABC",
+// //     año: 1998,
+// //     genero: "filosofia"
+
+// // };
+// // // CONSTANTES 
+
+// // //const NOMBRE_USUARIO = "Magali";
+// // //*
+// // x = 1;
+// // alert("x =" + x);
+// // let x;
+
+// // // var
+// // var nombre = "Marcus";
+// // console.log(nombre)
+
+// // if (true){
+// //     var nombre = "Dominic";
+// //     console.log(nombre);
+// // }
+// //  // let
+// // console.log(nombre);
+
+// // var edad = 45;
+// // console.log(edad);
+
+// // //if (true){
+// //     let edad = 40;
+// //     console.log(edad)
+// // }
+
+// //console.log(edad);
+
+// let nombre = "Alberto"
+// let edad = 22;
+// let condicion = true;
+
+// let edadString = String(edad);
+
+// let nombreNumerico = Number(nombre);
